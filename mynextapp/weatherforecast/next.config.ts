a6e306eb47b4d0a55edea9f655ab76f92b6b9f65
Davi <davilinfo@gmail.com>
2026-07-13 10:42:10 -0300

    nextjs applied and good

---
+++ mynextapp/weatherforecast/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  //reactCompiler: true
+    // Use the new React 18 compiler for faster builds and smaller bundles
+    // This is an experimental feature and may change in future releases
+};
+
+export default nextConfig;
